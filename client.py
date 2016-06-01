d015df613603525929ef07d81c8ad143f52f31e5
kestingj <kestingjoseph@gmail.com>
2016-05-31 18:33:06 -0700

    finished RegServiceWrapper

---
+++ client.py
@@ -0,0 +1,95 @@
+from agent import RegistrationAgent
+import sys
+import socket
+import os
+
+
+def main():
+	argv = sys.argv
+
+	if len(argv) != 3:
+		sys.exit()
+
+	addr = socket.gethostbyname(argv[1])	
+	agent = RegistrationAgent(addr, int(argv[2]))
+
+	print "regServerIP = {}".format(addr)
+	# print "thisHostIP = {}".format(socket.gethostbyname(socket.gethostname()))
+
+	while True:
+		print "in loop"
+		userInput = raw_input("Enter r(egister), u(nregister), f(etch), p(robe), or q(uit): ")
+		print userInput
+		readStdIn(userInput, agent)	
+
+def readStdIn(data, agent):
+	if data is not None and data.strip() is not "":
+		params = data.split()
+		command = params[0]
+		if command == "r":
+			register(params, agent)	
+		elif command == "u":
+			unregister(params, agent)		
+		elif command == "f":
+			fetch(params, agent)		
+		elif command == "p":
+			probe(agent)	
+		elif command == "q":
+			quit(agent)
+		else:
+			print "Unrecognized command"
+
+def register(params, agent):
+	if len(params) != 4:
+		print "Incorrect number of arguments"
+	else:
+		port = int(params[1])
+		servData = int(params[2])
+		name = params[3]
+		if port >= 0 and port < 65535:
+			lifetime = agent.register(port, servData, name)
+			if lifetime > 0:
+				# localAddr = socket.gethostbyname(socket.gethostname())
+				localAddr = "127.0.0.1" #DELETE
+				print "Register {}:{} successful: lifetime = {}".format(localAddr, port, lifetime)
+			else:
+				print "Registration failed"	
+		else: 
+			print "Port must be 0-65534."
+
+def fetch(params, agent):
+	responses = None
+	if len(params) > 2:
+		print "Incorrect number of arguments"
+	elif len(params) == 2:
+		responses = agent.fetch(params[1])
+	else:
+		responses = agent.fetch("")	
+	if responses is not None:
+		for i in range(len(responses)):
+			response = responses[i]
+			ip, port, serviceData = response
+			print "[{}] {} {} {}".format(i, ip, port, serviceData)
+	else: 
+		print "No entries were fetched"	
+
+def unregister(params, agent):
+	if len(params) != 2:
+		print "Incorrect number of arguments"
+	else:	
+		port = int(params[1])
+		agent.unregister(port)	
+
+def probe(agent):
+	success = agent.probe()
+	if success:
+		print "Success"
+	else:
+		print "Failure"
+
+def quit(agent):
+	agent.close()
+	os._exit(1)	
+
+if __name__ == "__main__":
+	main()
---
+++ src/RegistrationAgentWrapper.java
@@ -0,0 +1,112 @@
+import org.python.core.*;
+import org.python.util.PythonInterpreter;
+
+import java.io.*;
+import java.math.BigInteger;
+
+/**
+ * Created by josephkesting on 5/31/16.
+ */
+public class RegistrationAgentWrapper {
+
+    public static void main(String[] args) {
+        try {
+            RegistrationAgentWrapper reg = new RegistrationAgentWrapper("\"cse461.cs.washington.edu\"", 46101);
+            System.out.println(reg.probe());
+            reg.register(1234, 14000, "hermanos");
+            System.out.println(reg.fetch("")[0].getData());
+            reg.quit();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("HERE");
+        }
+    }
+
+    Process process;
+    BufferedWriter out;
+    BufferedReader in;
+    PythonInterpreter interpreter;
+    PyObject agent;
+
+    public RegistrationAgentWrapper (String host, int port) throws IOException {
+        ProcessBuilder processBuilder = new ProcessBuilder("python", "client.py", host, "" + port);
+        processBuilder.redirectErrorStream(true);
+        process = processBuilder.start();
+        out = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        System.out.println(in);
+        System.out.println(out);
+        interpreter = new PythonInterpreter();
+        interpreter.execfile("agent.py");
+        agent = interpreter.eval("RegistrationAgent("+host+", "+port+")");
+    }
+
+    public void register(int port, int data, String name) throws IOException {
+        PyObject[] args = {new PyInteger(port), new PyInteger(data), new PyString(name)};
+        agent.invoke("register", args);
+
+    }
+
+    public FetchResult[] fetch(String prefix) throws IOException {
+        PyList res = (PyList)agent.invoke("fetch", new PyString(prefix));
+        FetchResult[] results = new FetchResult[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            PyList itemLine = (PyList)res.get(i);
+            String ip = (String) itemLine.get(0);
+            int port = (int) itemLine.get(1);
+            int data = ((BigInteger) itemLine.get(2)).intValue();
+            FetchResult item = new FetchResult(ip, port, data);
+            results[i] = item;
+        }
+        return results;
+    }
+
+    public boolean probe() throws IOException {
+        PyBoolean res = (PyBoolean)agent.invoke("probe");
+
+        return res.getBooleanValue();
+    }
+
+    public void unregister(int port) throws IOException {
+        agent.invoke("unregister", new PyInteger(port));
+    }
+
+    public void quit() throws IOException {
+        agent.invoke("close");
+    }
+
+    public class FetchResult {
+        private String ip;
+        private int port;
+        private int data;
+
+        public FetchResult (String input) {
+            parseInput(input);
+        }
+
+        public FetchResult (String ip, int port, int data) {
+            this.ip = ip;
+            this.port = port;
+            this.data = data;
+        }
+
+        private void parseInput(String input) {
+            String[] splitInput = input.split(" ");
+            ip = splitInput[1];
+            port = Integer.parseInt(splitInput[2]);
+            data = Integer.parseInt(splitInput[3]);
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public int getData() {
+            return data;
+        }
+    }
+}

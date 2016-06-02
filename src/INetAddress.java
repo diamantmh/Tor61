bf02c145635cb53deee93c265d6ab9e1aed9182a
Michael Diamant <diamantmh@gmail.com>
2016-06-01 22:08:21 -0700

    added empty files should be good now

--- src/INetAddress.java
+++ src/INetAddress.java
@@ -2,4 +2,19 @@
  * Created by michaeldiamant on 6/1/16.
  */
 public class INetAddress {
+    private String host;
+    private int port;
+
+    public INetAddress(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
 }
